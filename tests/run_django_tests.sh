29972be3b8584f9690731da0993bb0a612a217b0
Mark Lee <oath-toolkit+python@lazymalevolence.com>
2014-02-05 12:34:04 -0800

    Travis CI: properly set the PYTHONPATH when running the Django tests if pyoath-toolkit not installed

--- tests/run_django_tests.sh
+++ tests/run_django_tests.sh
@@ -3,8 +3,11 @@
 RUN_CMD="`which django-admin.py` test oath_toolkit.django_otp"
 [[ "$1" == "COVERAGE" ]] && RUN_CMD="coverage run $RUN_CMD"
 
-cd `dirname $0`/..
-PYTHONPATH=tests DJANGO_SETTINGS_MODULE=test_django.settings $RUN_CMD
+BASE_DIR=`dirname $0`/..
+cd "$BASE_DIR"
+PPATH="$BASE_DIR/tests"
+[[ -n "$PYOTK_UNINSTALLED" ]] && PPATH="$PPATH:$BASE_DIR"
+PYTHONPATH="$PPATH" DJANGO_SETTINGS_MODULE=test_django.settings $RUN_CMD
 EXIT=$?
 [[ "$1" == "COVERAGE" ]] && coverage report -m
 exit $EXIT
